da84f5c994385985d943ada7de1ae45b62a8216b
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-12-21 12:14:05 +0800

    updates

--- pythonWebsite.py
+++ pythonWebsite.py
@@ -2,10 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import Keys
 
 def navigatePython():
-    global driver
     driver = webdriver.Chrome(service=ChromeService(
         ChromeDriverManager().install()))
 
@@ -13,8 +12,18 @@ def navigatePython():
 
     driver.maximize_window()
 
-    # while(True):
-    #     pass
+    # get the search bar element by name
+    element = driver.find_element(by=By.NAME, value="q")
+
+    # clear the search bar and enter Pycon and search
+    element.clear()
+    element.send_keys("ytteadjsajfkh")
+    element.send_keys(Keys.RETURN)
+
+    assert  "No results found." not in driver.page_source
+
+    while(True):
+        pass
 
 
 if __name__ == "__main__":
